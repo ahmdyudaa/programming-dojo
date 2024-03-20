293409ed2168e240af3f54258021ed718b549b0c
ahmdyudaa <ahmadyuda.hermawan05@gmail.com>
2024-03-20 16:24:14 +0700

    pertemuan 2 bug buster

---
+++ pertemuan_2/BugBuster.java
@@ -0,0 +1,8 @@
+class BugBuster {
+    public static void main(String[] args) {
+        int bilangan;
+        bilangan = 5;
+        //menampilkan variabel
+        System.out.println(bilangan);
+    }
+}
---
+++ pertemuan_2/Main.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args) {
+        // int umur = 25000000000000000000000000000000;
+        // long umur2 = umur;
+
+        float pi = 3.14f;
+        int piInt = (int)pi;
+        System.out.println(piInt);
+    }
+}
